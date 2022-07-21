aea578a0906f5f060cd1b6d52ac5dd47caf73ddd
Andrey <fail4iks@gmail.com>
2022-07-21 14:50:11 +0300

    Add Provider, thunkMiddleware

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -10,10 +10,6 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-
                 <HashRouter>
                     <Header/>
                     <Router/>
--- src/pages/app/store.ts
+++ src/pages/app/store.ts
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {authReducer} from "../auth/auth-reducer";
+import thunk from "redux-thunk";
 
 const reducers = combineReducers({
     auth: authReducer
 })
-export const store = createStore(reducers)
+export const store = createStore(reducers, applyMiddleware(thunk))
 export type AppStoreType = ReturnType<typeof reducers>
