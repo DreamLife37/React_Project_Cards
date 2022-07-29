1a6466a86387ce26a153e5b721602f932425fd59
gogy9000 <goropashnyj9000@gmail.com>
2022-07-29 20:54:22 +0500

    fix route

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -18,25 +18,29 @@ export enum Path {
     restorePassword = '/restore-password',
     setNewPassword = '/set-new-password',
     example = '/example',
-    redirectAfterSendRecoveryPassEmail='/redirect-page',
-    settings='/settings'
+    redirectAfterSendRecoveryPassEmail = '/redirect-page',
+    settings = '/settings'
 }
 
 export const Router = () => {
     return (
         <div>
             <Routes>
-                <Route path={Path.login} element={<Login/>}/>
-                <Route path={Path.registration} element={<RegistrationPage/>}/>
-                <Route path={Path.profile} element={<Profile/>}/>
-                <Route path={Path.error404} element={<Error404/>}/>
-                <Route path={Path.restorePassword} element={<RestorePassword/>}/>
-                <Route path={Path.setNewPassword} element={<SetNewPassword/>}>
-                    <Route path=':token' element={<SetNewPassword/>}/>
+                <Route path={'/'} element={<Profile/>}>
+                    <Route path={'/React_Project_for_Friday'} element={<Profile/>}>
+                        <Route path={Path.login} element={<Login/>}/>
+                        <Route path={Path.registration} element={<RegistrationPage/>}/>
+                        <Route path={Path.profile} element={<Profile/>}/>
+                        <Route path={Path.error404} element={<Error404/>}/>
+                        <Route path={Path.restorePassword} element={<RestorePassword/>}/>
+                        <Route path={Path.setNewPassword} element={<SetNewPassword/>}>
+                            <Route path=':token' element={<SetNewPassword/>}/>
+                        </Route>
+                        <Route path={Path.example} element={<Example/>}/>
+                        <Route path={Path.redirectAfterSendRecoveryPassEmail} element={<RedirectPage/>}/>
+                        <Route path={Path.settings} element={<Settings/>}/>
+                    </Route>
                 </Route>
-                <Route path={Path.example} element={<Example/>}/>
-                <Route path={Path.redirectAfterSendRecoveryPassEmail} element={<RedirectPage/>}/>
-                <Route path={Path.settings} element={<Settings/>}/>
             </Routes>
         </div>
     )
