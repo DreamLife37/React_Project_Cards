a4917f620423149d0a1cec1f4d2239adf49114c0
gogy9000 <goropashnyj9000@gmail.com>
2022-08-01 18:57:10 +0500

    create Search feature

--- src/pages/packsList/PackReducer.ts
+++ src/pages/packsList/PackReducer.ts
@@ -9,58 +9,67 @@ import {
 import {AppDispatchType, AppThunk} from "../app/store";
 import {HandleToggleStatusAppAndInterceptorErrors} from "../../utils/HandleToggleStatusAppAndInterceptorErrors";
 
-const initialState={
-    packsData:{} as GetCardsPackResponse,
-    queryParams:{
-        packName:undefined,
-        min:undefined,
-        max:undefined,
-        page:undefined,
-        pageCount:undefined,
-        user_id:undefined,
-        sortPacks:undefined
+const initialState = {
+    packsData: {} as GetCardsPackResponse,
+    queryParams: {
+        packName: undefined,
+        min: undefined,
+        max: undefined,
+        page: undefined,
+        pageCount: undefined,
+        user_id: undefined,
+        sortPacks: undefined
     } as getCardPacksPayload
 }
 
 
-const packsSlice=createSlice({
-    name:"Packs",
+const packsSlice = createSlice({
+    name: "Packs",
     initialState,
-    reducers:{
-        getPack:(state,action)=>{
-            state.packsData=action.payload
+    reducers: {
+        getPack: (state, action) => {
+            state.packsData = action.payload
+        },
+        setQuery: (state, action) => {
+            state.queryParams = {...state.queryParams, ...action.payload}
         }
     }
 })
 
-export const packs=packsSlice.reducer
-export const actionsPacks=packsSlice.actions
+export const packs = packsSlice.reducer
+export const actionsPacks = packsSlice.actions
 
-export const thunksPack={
-    getPack:(responseMore?:any):AppThunk=>(dispatch:AppDispatchType,getState)=>{
+export const thunksPack = {
+    getPack: (responseMore?: any): AppThunk => (dispatch: AppDispatchType, getState) => {
         Promise.allSettled([responseMore])
-            .then(()=>{
-                const response= APIPacks.getCardPacks(getState().packs.queryParams)
-                    .then((response:GetCardsPackResponse)=>{
+            .then(() => {
+                const response = APIPacks.getCardPacks(getState().packs.queryParams)
+                    .then((response: GetCardsPackResponse) => {
                             dispatch(actionsPacks.getPack(response))
                         }
                     )
-                HandleToggleStatusAppAndInterceptorErrors(dispatch, [response,responseMore])
+                HandleToggleStatusAppAndInterceptorErrors(dispatch, [response, responseMore])
             })
     },
 
-    createPack:(payload:CreateNewCardPackPayload):AppThunk=>(dispatch:AppDispatchType)=>{
-     const response= APIPacks.createNewCardPack(payload)
+    createPack: (payload: CreateNewCardPackPayload): AppThunk => (dispatch: AppDispatchType) => {
+        const response = APIPacks.createNewCardPack(payload)
         dispatch(thunksPack.getPack(response))
     },
 
-    updatePack:(payload:UpdateCardPackPayload):AppThunk=> (dispatch:AppDispatchType)=>{
-        const response= APIPacks.updateCardPack(payload)
+    updatePack: (payload: UpdateCardPackPayload): AppThunk => (dispatch: AppDispatchType) => {
+        const response = APIPacks.updateCardPack(payload)
         dispatch(thunksPack.getPack(response))
     },
 
-    deletePack:(packId:string):AppThunk=>(dispatch:AppDispatchType)=>{
-        const response=APIPacks.deleteCardPack(packId)
+    deletePack: (packId: string): AppThunk => (dispatch: AppDispatchType) => {
+        const response = APIPacks.deleteCardPack(packId)
         dispatch(thunksPack.getPack(response))
+    },
+    searchOnName: (packName: string): AppThunk => (dispatch: AppDispatchType) => {
+        //ни чего больше писать не надо все случится автоматически
+        //нужно только поменять в стейте квери параметр и вызвать санку getPack
+        dispatch(actionsPacks.setQuery({packName}))
+        dispatch(thunksPack.getPack())
     }
 }
