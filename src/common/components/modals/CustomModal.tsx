4e72f4103a41a5b89d2ca99d1234308d1aaf3edd
Andrey <fail4iks@gmail.com>
2022-08-07 22:22:22 +0300

    Added modal for adding and editing pack

---
+++ src/common/components/modals/CustomModal.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import {FC, ReactElement} from "react";
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+type PropsType = {
+    children: ReactElement
+    handleClose: () => void
+    open: boolean
+
+}
+
+export const CustomModal: FC<PropsType> = ({children, handleClose, open}) => {
+    return (
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    {children}
+                </Box>
+            </Modal>
+        </div>
+    );
+}
--- src/pages/packsList/PacksListPage.tsx
+++ src/pages/packsList/PacksListPage.tsx
@@ -1,16 +1,17 @@
 import {Search} from "./Search";
 import {FilterMyPacks} from "./FilterMyPacks";
 import {NumberOfCards} from "./NumberOfCards";
-import {TablePacks} from "./TablePacks";
 import {Grid} from "@mui/material";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 import {useDispatchApp, useSelectorApp} from "../../CustomHooks/CustomHooks";
-import {useEffect} from "react";
+import {useState} from "react";
 import {thunksPack} from "./PackReducer";
 import {styled} from "@mui/material/styles";
 import {TablePacks1} from "./TablePacks1";
+import {ModalFormikPackType} from "./modals/FormikFormModal";
+import { AddAndEditPackModal } from "./modals/AddAndEditPackModal";
 
 export const PacksListPage = () => {
 
@@ -20,22 +21,29 @@ export const PacksListPage = () => {
 
     const dispatch = useDispatchApp()
 
-    const AddNewPack = () => {
-        dispatch(thunksPack.createPack({name: 'програмист!'}))
+    const [open, setOpen] = useState(false)
+    const handleOpen = (): void => setOpen(true)
+    const handleClose = (): void => setOpen(false)
+
+
+    const addNewPack = (e: ModalFormikPackType) => {
+        dispatch(thunksPack.createPack({name: e.namePack}))
     }
 
     const StyledButton = styled(Button)`
       color: #050505;
       border-radius: 30px;
     `
-
     return (
         <Grid container spacing={2} justifyContent='center' columnSpacing={{xs: 1, sm: 2, md: 3}}>
             <Grid container alignItems="center" direction="row"
-                  justifyContent="center" paddingTop={'40px'}><Grid item xs={6}>PacksList</Grid>
+                  justifyContent="center" paddingTop={'40px'}>
+                <Grid item xs={6}>PacksList</Grid>
                 {isAuthorized &&
                     <Grid item xs={6}>
-                        <StyledButton onClick={AddNewPack} color="inherit" variant='contained'>
+                        <AddAndEditPackModal callback={addNewPack} handleClose={handleClose} open={open}
+                                         title={'Add new pack'}/>
+                        <StyledButton color="inherit" variant='contained' onClick={handleOpen}>
                             Add new pack
                         </StyledButton>
                     </Grid>}
