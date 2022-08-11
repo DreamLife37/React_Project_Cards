61a4e2ccd9cf19af0057df14c308e17507d54852
gogy9000 <goropashnyj9000@gmail.com>
2022-08-11 19:46:05 +0500

    install and use axios-rate-limit, and fixed update thunk, and fix rating

--- src/DAL/Global.ts
+++ src/DAL/Global.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import axiosRateLimit from "axios-rate-limit";
 
-export const instance = axios.create({
+export const instance = axiosRateLimit( axios.create({
      baseURL:'https://neko-back.herokuapp.com/2.0',
     //baseURL: 'http://localhost:7542/2.0/',
     withCredentials: true,
-})
+}),{maxRequests:1,perMilliseconds:3000,maxRPS:1})
