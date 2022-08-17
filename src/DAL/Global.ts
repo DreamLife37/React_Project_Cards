b87282fbc20ecb639e43fe6599c12efbbc1b0117
Andrey <fail4iks@gmail.com>
2022-08-17 11:40:19 +0300

    Added conversion file to base64 and upload avatar

--- src/DAL/Global.ts
+++ src/DAL/Global.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import axiosRateLimit from "axios-rate-limit";
 
 export const instance = axiosRateLimit( axios.create({
-     baseURL:'https://neko-back.herokuapp.com/2.0',
-    //baseURL: 'http://localhost:7542/2.0/',
+     //baseURL:'https://neko-back.herokuapp.com/2.0',
+    baseURL: 'http://localhost:7542/2.0/',
     withCredentials: true,
 }),{maxRequests:1,perMilliseconds:3000,maxRPS:1})
